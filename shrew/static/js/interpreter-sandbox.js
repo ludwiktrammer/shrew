20b92d98360329d9a1cbd3e190271c8d885e116d
Ludwik Trammer <ludwik@gmail.com>
2018-06-23 19:56:53 +0200

    Replace Brython with Skulpt

--- shrew/static/js/interpreter-sandbox.js
+++ shrew/static/js/interpreter-sandbox.js
@@ -1,43 +1,31 @@
-(function($B){
-    // Initiate Brython
-    $B.brython({
-        ipy_id: [],
-        static_stdlib_import: false,
-        pythonpath: ["/static/python"],
-    });
+import Sk from "skulpt";
 
-    // Remove modules for interacting with the browser
-    delete $B.imported["browser"];
-    delete $B.imported["browser.html"];
-    delete $B.imported["javascript"];
+Sk.configure({
+    output: output,
+    read: builtinRead,
+    python3: true,
+});
+window.addEventListener("message", runCode);
 
-    window.addEventListener("message", runCode);
 
-    function runCode(event) {
-        if (event.origin !== location.origin) {
-            // If the message came from a different domain
-            // ignore it for security reasons.
-            return;
-        }
+function output(x) {
+    console.log(x);
+}
 
-        // Add implicit imports
-        let pythonCode = "from drawing import *\n\n" + event.data.code;
-
-        // $B.$py_module_path['__main__'] = $B.brython_path;
-        try {
-            let js = $B.py2js(pythonCode, "__main__", "__main__", $B.builtins_scope).to_js();
-            eval(js);
-        } catch($err) {
-            // If the error was not caught by the Python runtime, build an
-            // instance of a Python exception
-            if($err.$py_error === undefined){
-                console.log('Javascript error', $err);
-                $err = $B.builtins.RuntimeError.$factory($err + '')
-            }
-
-            let $trace = $B.builtins.getattr($err,'info') + '\n' + $err.__class__.__name__ +
-                ': ' + $err.args;
-            console.log($trace);
-        }
+function runCode(event) {
+    if (event.origin !== location.origin) {
+        // If the message came from a different domain
+        // ignore it for security reasons.
+        return;
     }
-})(__BRYTHON__);
+
+    Sk.misceval.asyncToPromise(function() {
+        return Sk.importMainWithBody("<stdin>", false, event.data.code, true);
+    });
+}
+
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+        throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
