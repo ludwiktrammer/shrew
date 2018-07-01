425c64174fdc6110b1b21555c77298b4a8dc2b4b
Ludwik Trammer <ludwik@gmail.com>
2018-07-01 18:27:37 +0200

    Add admin's email address to production settings

--- shrew/settings/prod.py
+++ shrew/settings/prod.py
@@ -10,3 +10,7 @@ SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_SECONDS = 60 * 60 * 24 * 365 * 5  # 5 years
 
 STATIC_ROOT = env.str("DJANGO_STATIC_ROOT")
+
+ADMINS = [
+    ('Ludwik', 'ludwik@gmail.com'),
+]
