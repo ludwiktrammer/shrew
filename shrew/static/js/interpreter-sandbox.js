dbecfbafe8bafd1a4f8b8445d6db9707d303fe38
Ludwik Trammer <ludwik@gmail.com>
2018-07-05 20:29:23 +0200

    Even a smaller execution time limit

--- shrew/static/js/interpreter-sandbox.js
+++ shrew/static/js/interpreter-sandbox.js
@@ -7,7 +7,7 @@ Sk.configure({
     uncaughtException: output,
     read: builtinRead,
     python3: true,
-    execLimit: 1500,
+    execLimit: 750,
 });
 window.addEventListener("message", runCode);
 window.parent.postMessage({type: "interpreter-ready"}, "*");
