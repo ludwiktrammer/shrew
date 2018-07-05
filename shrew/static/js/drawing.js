ee98939b3a004f3a18c7b319a61dc9cb3fabe49f
Ludwik Trammer <ludwik@gmail.com>
2018-07-05 22:53:00 +0200

    Refactor drawing functions

--- shrew/static/js/drawing.js
+++ shrew/static/js/drawing.js
@@ -7,13 +7,86 @@ let draw = SVG('drawing').viewbox(0, 0, 100, 100);
 // per animation block. So we keep past animation objects here.
 let animationCache = {};
 let timeoutObject;
+let animation;
+let hasAnimation;
+
+// Specific handlers for different types of action
+let actionHandlers = {
+    "default": (shapeId, command, value, initial) => {
+        let shape = getShape(shapeId, (animation && !initial));
+        shape[command](value);
+    },
+
+    "created": (shapeId, command, value) => {
+        let shape = draw[value[0]](...value.slice(1)).id(shapeId);
+        if (animation) {
+            shape.opacity(0);
+        }
+    },
+
+    "animation": (shapeId, command, value) => {
+        hasAnimation = true;
+        animation = {
+            duration: value[0] * 1000,
+        };
+    },
+
+    "animation-end": () => {
+        let animationObj = Object.values(animationCache)[0];
+        let duration = animation.duration;
+
+        return new Promise((resolve, reject) => {
+            if (animationObj !== undefined) {
+                animationObj.after(resolve);
+            } else {
+                timeoutObject = setTimeout(resolve, duration);
+            }
+        }).then(() => {
+            animation = undefined;
+            animationCache = {};
+        });
+    },
+
+    "attr": (shapeId, command, value, initial) => {
+        let shape = getShape(shapeId, animation=(animation && !initial));
+        shape[command](...value);
+    },
+
+    "font": (...args) => {
+        actionHandlers.attr(...args);
+    },
+
+    "fill": (shapeId, command, value) => {
+        let shape = getShape(shapeId);
+        if (!Array.isArray(value)) {
+            value = [value];
+        }
+        let existingGradient = shape.reference(command);
+        if (existingGradient) {
+            updateGradient(value, existingGradient, animation);
+        } else {
+            let gradient = createGradient(value);
+            shape[command](gradient);
+        }
+    },
+
+    "stroke": (...args) => {
+        actionHandlers.fill(...args);
+    },
+
+    "opacity": (shapeId, command, value) => {
+        let shape = getShape(shapeId, !!animation);
+        shape[command](value);
+    },
+};
+
 
 export function drawFromActions(actions) {
     draw.clear();
     cancelAnimations();
+    animation = undefined;
+    hasAnimation = false;
 
-    let animation;
-    let hasAnimation = false;
     processAction(actions, 0);
     function processAction(actions, index) {
         if (index >= actions.length) {
@@ -25,64 +98,39 @@ export function drawFromActions(actions) {
         }
         let [shapeId, command, value, initial] = actions[index];
 
-        if (command === "animation-end") {
-            let animationObj = Object.values(animationCache)[0];
-            let duration = animation.duration;
-
-            function afterAnimationFinished() {
-                animation = undefined;
-                animationCache = {};
-                processAction(actions, ++index);
-            }
-
-            if (animationObj !== undefined) {
-                animationObj.after(afterAnimationFinished);
-            } else {
-                timeoutObject = setTimeout(afterAnimationFinished, duration);
-            }
-            return;
-        }
-        if (command === "created") {
-            let shape = draw[value[0]](...value.slice(1)).id(shapeId);
-            if (animation) {
-                shape.opacity(0);
-            }
-        } else if (command === "animation") {
-            hasAnimation = true;
-            animation = {
-                duration: value[0] * 1000,
-            };
-        } else if(['fill', 'stroke'].includes(command)) {
-            // gradient support
-            let shape = SVG.get(shapeId);
-            if (!Array.isArray(value)) {
-                value = [value];
-            }
-            let existingGradient = shape.reference(command);
-            if (existingGradient) {
-                updateGradient(value, existingGradient, animation);
-            } else {
-                let gradient = createGradient(value);
-                shape[command](gradient);
-            }
+        let handler_promise;
+        if(command  in actionHandlers) {
+            handler_promise = actionHandlers[command](shapeId, command, value, initial);
         } else {
-            let shape = SVG.get(shapeId);
-            if (animation && (!initial || command === "opacity")) {
-                if (animationCache[shapeId]) {
-                    shape = animationCache[shapeId];
-                } else {
-                    shape = shape.animate(animation.duration);
-                    animationCache[shapeId] = shape;
-                }
-            }
-            if (['attr', 'font'].includes(command)) {
-                shape[command](...value);
-            } else {
-                shape[command](value);
-            }
+            handler_promise = actionHandlers["default"](shapeId, command, value, initial);
         }
-        processAction(actions, ++index);
+
+        if (handler_promise !== undefined) {
+            // if handler returned a promise, wait for it
+            handler_promise.then(() => {
+                processAction(actions, ++index);
+            })
+        } else {
+            processAction(actions, ++index);
+        }
     }
+}
+
+/**
+ * Returns the shape object or (if `isAnimated` is set to true)
+ * an animation object associated with it.
+ */
+function getShape(shapeId, isAnimated=false) {
+    let shape = SVG.get(shapeId);
+    if (isAnimated) {
+        if (animationCache[shapeId]) {
+            shape = animationCache[shapeId];
+        } else {
+            shape = shape.animate(animation.duration);
+            animationCache[shapeId] = shape;
+        }
+    }
+    return shape;
 }
 
 function cancelAnimations() {
@@ -113,7 +161,6 @@ function updateGradient(colors, gradient, animation) {
         let color = getColorFromName(colors[i]);
         stop = gradient.get(i);
         if(stop === null) {
-            console.log(`created ${i}`);
             stop = gradient.at(1, color);
         }
         if (animation) {
