96469d8fb52ecf41fc554930df3921e60a86af39
Ludwik Trammer <ludwik@gmail.com>
2018-07-20 21:33:28 +0200

    Add an outdated browser warning

---
+++ shrew/static/js/outdated-browser.js
@@ -0,0 +1,15 @@
+import outdatedBrowserRework from 'outdated-browser-rework';
+
+outdatedBrowserRework({
+	browserSupport: {
+		'Chrome': 21,
+		'Edge': 12,
+		'Safari': 7,
+		'Mobile Safari': 8,
+		'Firefox': 28,
+		'Opera': 15,
+		'Vivaldi': 1,
+		'IE': 11,
+	},
+	isUnknownBrowserOK: true,
+});
