93dc8ebc5f14a3da9559990ff5aa066aa3a0aee0
Ludwik Trammer <ludwik@gmail.com>
2018-07-20 22:20:27 +0200

    Revert "Remove IE 11 compatibility (didn't work after all)"
    This reverts commit e4697dbc11ebf7ef81b546a3c104e85b90f0fe76.

---
+++ shrew/static/js/polyfill.js
@@ -0,0 +1,7 @@
+// Polyfills needed by IE 11
+import 'core-js/fn/symbol';
+import 'core-js/fn/symbol/iterator';
+import 'core-js/fn/object/values';
+import 'core-js/fn/array/includes';
+import 'core-js/fn/promise';
+import 'whatwg-fetch'
